0b7ffa2b77e73f8c3ea199fc462b41b9b19106b0
Kotturi858 <kotturi858bharath@gmail.com>
2025-05-25 23:29:50 +0530

    added toaster library package

---
+++ projects/kotturi/toaster/src/lib/toaster.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-toaster',
+  imports: [],
+  template: `
+    <p>
+      toaster works!
+    </p>
+  `,
+  styles: ``
+})
+export class ToasterComponent {
+
+}
---
+++ projects/kotturi/toaster/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of toaster
+ */
+
+export * from './lib/toaster.service';
+export * from './lib/toaster.component';
